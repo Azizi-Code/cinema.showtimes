b8e13aaaba406d1d8337ca97a940137ba20d24fe
Roohallah Azizi <roohallah.azizi@virtudev.net>
2023-11-13 15:00:54 +0300

    feat: create controller for showtimes

---
+++ src/Cinema.Showtimes.Api/Controllers/ShowtimesController.cs
@@ -0,0 +1,34 @@
+using Cinema.Showtimes.Api.Application.Services;
+using Cinema.Showtimes.Api.Infrastructure.ActionResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinema.Showtimes.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/showtimes")]
+public class ShowtimesController : Controller
+{
+    private readonly IMoviesService _moviesService;
+    private readonly IActionResultMapper<ShowtimesController> _actionResultMapper;
+    
+    public ShowtimesController(IMoviesService moviesService,
+        IActionResultMapper<ShowtimesController> actionResultMapper)
+    {
+        _moviesService = moviesService;
+        _actionResultMapper = actionResultMapper;
+    }
+
+    [HttpGet("id")]
+    public async Task<IActionResult> GetMovieById(string id)
+    {
+        try
+        {
+            var result = await _moviesService.GetByIdAsync(id);
+            return Ok(result);
+        }
+        catch (Exception exception)
+        {
+            return _actionResultMapper.Map(exception);
+        }
+    }
+}
