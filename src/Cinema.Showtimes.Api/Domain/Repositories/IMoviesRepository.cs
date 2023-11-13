34fa8ab99ce051000ca42f38e6661b831e3588a3
Roohallah Azizi <roohallah.azizi@virtudev.net>
2023-11-14 00:12:55 +0300

    feat: add repository for movie

---
+++ src/Cinema.Showtimes.Api/Domain/Repositories/IMoviesRepository.cs
@@ -0,0 +1,8 @@
+﻿using Cinema.Showtimes.Api.Domain.Entities;
+
+namespace Cinema.Showtimes.Api.Domain.Repositories;
+
+public interface IMoviesRepository
+{
+    Task<MovieEntity?> GetByIdAsync(int movieId, CancellationToken cancellationToken = default);
+}
---
+++ src/Cinema.Showtimes.Api/Infrastructure/Database/MoviesRepository.cs
@@ -0,0 +1,20 @@
+﻿using Cinema.Showtimes.Api.Domain.Entities;
+using Cinema.Showtimes.Api.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Showtimes.Api.Infrastructure.Database;
+
+public class MoviesRepository : IMoviesRepository
+{
+    private readonly CinemaContext _context;
+
+    public MoviesRepository(CinemaContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<MovieEntity?> GetByIdAsync(int movieId, CancellationToken cancellationToken)
+    {
+        return await _context.Movies.AsNoTracking().FirstOrDefaultAsync(x => x.Id == movieId, cancellationToken);
+    }
+}
