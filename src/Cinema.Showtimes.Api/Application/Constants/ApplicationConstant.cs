0c6f694cf503cac6d959e94338aa7319ed44d88e
Roohallah Azizi <roohallah.azizi@virtudev.net>
2023-11-16 12:56:03 +0300

    refactor: add movies api address to the app settings

--- src/Cinema.Showtimes.Api/Application/Constants/ApplicationConstant.cs
+++ src/Cinema.Showtimes.Api/Application/Constants/ApplicationConstant.cs
@@ -2,8 +2,8 @@ namespace Cinema.Showtimes.Api.Application.Constants;
 
 public static class ApplicationConstant
 {
-    public const string ApiKeyHeaderName = "X-Apikey";
     public const string ApiKeyName = "MoviesApiKey";
-    public const string MovieApiAddress = "https://localhost:7443";
+    public const string ApiKeyHeaderName = "X-Apikey";
+    public const string MovieApiAddressKey = "MoviesApiUrl";
     public const string RedisConnectionKey = "RedisConnection";
 }
